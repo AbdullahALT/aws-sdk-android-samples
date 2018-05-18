e9bce4c8e96c98c167316d0f3296e8eb260d1b1b
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2018-05-18 13:54:17 +0300

    Configure and Update the KinesisVideoDema App

--- AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/activity/StartUpActivity.java
+++ AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/activity/StartUpActivity.java
@@ -19,6 +19,36 @@ public class StartUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+        IdentityManager.getDefaultIdentityManager().resumeSession(this, new StartupAuthResultHandler() {
+            @Override
+            public void onComplete(StartupAuthResult authResults) {
+                if (authResults.isUserSignedIn()) {
+                    ActivityUtils.startActivity(StartUpActivity.this, SimpleNavActivity.class);
+                } else {
+                    IdentityManager.getDefaultIdentityManager().login(StartUpActivity.this, new DefaultSignInResultHandler() {
+                        @Override
+                        public void onSuccess(Activity callingActivity, IdentityProvider provider) {
+                            ActivityUtils.startActivity(StartUpActivity.this, SimpleNavActivity.class);
+                        }
+
+                        @Override
+                        public boolean onCancel(Activity callingActivity) {
+                            return false; // false = User cannot cancel login
+                        }
+                    });
+                    SignInActivity.startSignInActivity(StartUpActivity.this, new AuthUIConfiguration.Builder().userPools(true).build());
+                }
+            }
+        });
+    }
+
+    /**
+     * This code was used in the example, I changed it because it uses deprecated functions.
+     * Using these deprecated functions seems to make the application behave in an unexpected way
+     */
+    private void oldCode(){
         IdentityManager.getDefaultIdentityManager().doStartupAuth(this, new StartupAuthResultHandler() {
             @Override
             public void onComplete(StartupAuthResult authResults) {
